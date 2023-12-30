102d54f22fd3c6e6d9522f9cbe3c2cb36b509d0d
Mamen Vidal <mvidalda@uoc.edu>
2023-12-30 11:56:39 +0100

    add expo notifications

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { TouchableOpacity } from 'react-native';
@@ -7,10 +7,25 @@ import { MaterialIcons } from '@expo/vector-icons';
 import InicioScreen from './InicioScreen'; 
 import DetallesViajeScreen from './DetallesViajeScreen'; 
 import MultimediaScreenComponent from './MultimediaScreen'; 
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 
 const Stack = createStackNavigator();
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
 export default function App() {
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
   
   const styles = StyleSheet.create({
     container: {
@@ -24,54 +39,131 @@ export default function App() {
       marginRight: 10,
     },
   });
+  
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={({ navigation }) => ({
-          headerRight: () => (
-            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-              <View style={styles.iconContainer}>
-                <MaterialIcons name="home" size={30} color="white" />
-              </View>
-            </TouchableOpacity>
-          ),
-          headerStyle: {
-            backgroundColor: '#000',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        })}
-      >
-        <Stack.Screen
-          name="Home"
-          component={InicioScreen}
-          options={({ navigation }) => ({
-            title: 'misViajes',
-            headerStyle: {
-              backgroundColor: '#000',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            }, 
-          })}
+    <View style={{ flex: 1 }}>
+      {/* Bloque de notificaciones */}
+      <View style={{ alignItems: 'center', justifyContent: 'space-around' }}>
+        <Text>Your expo push token: {expoPushToken}</Text>
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Title: {notification && notification.request.content.title}</Text>
+          <Text>Body: {notification && notification.request.content.body}</Text>
+          <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+        </View>
+        <Button
+          title="Clic para recibir notificación"
+          onPress={async () => {
+            await schedulePushNotification();
+          }}
         />
-        <Stack.Screen 
-          name="DetallesViaje" 
-          component={DetallesViajeScreen} 
-          options={{ title: 'Detalle del día' }}
-        />
-        <Stack.Screen 
-          name="MultimediaScreen" 
-          component={MultimediaScreenComponent}
-          options={{ title: 'Vídeo del día' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+      </View>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={({ navigation }) => ({
+              headerRight: () => (
+                <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+                  <View style={styles.iconContainer}>
+                    <MaterialIcons name="home" size={30} color="white" />
+                  </View>
+                </TouchableOpacity>
+              ),
+              headerStyle: {
+                backgroundColor: '#000',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            })}
+          >
+            <Stack.Screen
+              name="Home"
+              component={InicioScreen}
+              options={({ navigation }) => ({
+                title: 'misViajes',
+                headerStyle: {
+                  backgroundColor: '#000',
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                  fontWeight: 'bold',
+                }, 
+              })}
+            />
+            <Stack.Screen 
+              name="DetallesViaje" 
+              component={DetallesViajeScreen} 
+              options={{ title: 'Detalle del día' }}
+            />
+            <Stack.Screen 
+              name="MultimediaScreen" 
+              component={MultimediaScreenComponent}
+              options={{ title: 'Vídeo del día' }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+    </View>
     
   );
   
+}
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "Tienes un mensaje! 📬",
+      body: 'Aquí va el mensaje de la notificación.',
+      data: { data: 'funciona!' },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  return token;
 }
